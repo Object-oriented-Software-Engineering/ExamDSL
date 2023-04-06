7debb07120b623338780b3eccd91de9395dc69e5
Grigoris Dimitroulakos <dhmhgre@gmail.com>
2023-04-06 11:05:08 +0300

    Commit contain main DSL symbol classes without being tested

---
+++ ExamDSL/DSLConcreteSymbols.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum ExamSymbolType {
+    ST_EXAMBUILDER, ST_EXAMHEADER, ST_EXAMQUESTION,
+    ST_COMPOSITETEXT, ST_STATICTEXT
+}
+
+namespace ExamDSL {
+    // Exam : Header? Questions
+    internal class ExamBuilder : ASTComposite {
+        // contexts
+        public const int HEADER=0, QUESTIONS=1;
+
+        private ExamBuilder() : 
+            base(2,(int)(ExamSymbolType.ST_EXAMBUILDER)) {
+
+        }
+        public static ExamBuilder exam() {
+            return new ExamBuilder();
+        }
+        public ExamBuilder header() {
+            var headerBuilder = new ExamHeaderBuilder();
+            AddChild(HEADER,headerBuilder);
+            return this;
+        }
+    }
+
+    // ExamHeader : Title? Semester? Date? Duration? Teacher? StudentName?
+    internal class ExamHeaderBuilder :ASTComposite {
+        public const int TITLE =0, SEMESTER=1, DATE=2, DURATION=3,TEACHER=4,STUDENTNAME=5;
+        public ExamHeaderBuilder() : 
+            base(6, (int)ExamSymbolType.ST_EXAMHEADER) { }
+
+        public ExamHeaderBuilder Title(DSLSymbol content) {
+            AddChild(TITLE,content);
+            return this;
+        }
+        public ExamHeaderBuilder Semester(DSLSymbol content) {
+            AddChild(SEMESTER, content);
+            return this;
+        }
+        public ExamHeaderBuilder Date(DSLSymbol content) {
+            AddChild(DATE, content);
+            return this;
+        }
+        public ExamHeaderBuilder Duration(DSLSymbol content) {
+            AddChild(DURATION, content);
+            return this;
+        }
+        public ExamHeaderBuilder Teacher(DSLSymbol content) {
+            AddChild(TEACHER, content);
+            return this;
+        }
+        public ExamHeaderBuilder StudentName(DSLSymbol content) {
+            AddChild(STUDENTNAME, content);
+            return this;
+        }
+        public ExamBuilder End() {
+            return MParent as ExamBuilder;
+        }
+    }
+
+    // Question : Header Gravity Wording Solution Subquestions? 
+    internal class ExamQuestionBuilder : ASTComposite {
+        public const int HEADER = 0, GRAVITY = 1, WORDING = 2, SOLUTION = 3, SUBQUESTION = 4;
+
+        public ExamQuestionBuilder() : 
+            base(5, (int)ExamSymbolType.ST_EXAMQUESTION) { }
+
+        public ExamQuestionBuilder Header(DSLSymbol content) {
+            AddChild(HEADER, content);
+            return this;
+        }
+        public ExamQuestionBuilder Gravity(DSLSymbol content) {
+            AddChild(GRAVITY, content);
+            return this;
+        }
+        public ExamQuestionBuilder Wording(DSLSymbol content) {
+            AddChild(WORDING, content);
+            return this;
+        }
+        public ExamQuestionBuilder Solution(DSLSymbol content) {
+            AddChild(SOLUTION, content);
+            return this;
+        }
+        public ExamQuestionBuilder SubQuestion(DSLSymbol content) {
+            AddChild(SUBQUESTION, content);
+            return this;
+        }
+        public ExamBuilder End() {
+            return MParent as ExamBuilder;
+        }
+    }
+
+    internal class CompositeTextSymbol : ASTComposite {
+        public const int CONTENT = 0;
+        public CompositeTextSymbol() : 
+            base(1, (int)ExamSymbolType.ST_COMPOSITETEXT) { }
+
+        public CompositeTextSymbol Text(DSLSymbol content) {
+            AddChild(CONTENT, content);
+            return this;
+        }
+    }
+
+    internal class StaticTextSymbol : ASTLeaf {
+        public StaticTextSymbol(string content) :
+            base(content, (int)ExamSymbolType.ST_STATICTEXT) { }
+    }
+
+}
---
+++ ExamDSL/DSLSymbols.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamDSL {
+    public class DSLSymbol : IASTVisitableNode, ILabelled {
+        private int m_type;
+        private int m_serialNumber;
+        // open for modification by subclasses
+        protected string m_nodeName;
+        private ASTComposite m_parent;
+        private static int ms_serialCounter;
+        // Either will use this member or there has to be
+        // a static dictionary to map ASTNode to the corresponding
+        // link hierarchy node. Trees have many applications 
+        // most of the times they extend to an application domain
+        // So this link is necessary.
+        private object m_hierarchyBridgeLink;
+
+        public object HierarchyBridgeLink {
+            get => m_hierarchyBridgeLink;
+            set => m_hierarchyBridgeLink = value ??
+                throw new ArgumentNullException(nameof(value));
+        }
+
+        public int MType => m_type;
+
+        // Node label is open for changes as is virtual
+        public virtual int MSerialNumber => m_serialNumber;
+
+        // Node label is open for changes as is virtual
+        public virtual string MNodeName => m_nodeName;
+
+        public ASTComposite MParent => m_parent;
+
+        public static int MsSerialCounter => ms_serialCounter;
+
+        public DSLSymbol(int mType) {
+            m_type = mType;
+            m_serialNumber = ms_serialCounter++;
+            m_nodeName = "Node" + GetType().Name + m_serialNumber;
+            m_parent = null;
+        }
+
+        public virtual void SetParent(ASTComposite parent) {
+            m_parent = parent;
+        }
+
+        public virtual Return Accept<Return, Params>(IASTBaseVisitor<Return, Params> v,
+            params Params[] info) {
+            return v.Visit(this);
+        }
+    }
+
+    public class ASTComposite : DSLSymbol {
+
+        List<DSLSymbol>[] m_children;
+
+        public int MContexts => m_children.Length;
+
+        public ASTComposite(int contexts, int mType) :
+            base(mType) {
+            m_children = new List<DSLSymbol>[contexts];
+            for (int i = 0; i < contexts; i++) {
+                m_children[i] = new List<DSLSymbol>();
+            }
+        }
+
+        public int GetNumberOfContextNodes(int context) {
+            if (context < m_children.Length) {
+                return m_children[context].Count;
+            } else {
+                throw new ArgumentOutOfRangeException("context index out of range");
+            }
+        }
+
+        public IEnumerable<DSLSymbol> GetContextChildren(int context) {
+            if (context < m_children.Length) {
+                foreach (DSLSymbol node in m_children[context]) {
+                    yield return node;
+                }
+            } else {
+                throw new ArgumentOutOfRangeException("node index out of range");
+            }
+        }
+
+        
+        public DSLSymbol GetChild(int context, int index = 0) {
+            if (context < m_children.Length) {
+                if (index < m_children[context].Count) {
+                    return m_children[context][index];
+                } else {
+                    throw new ArgumentOutOfRangeException("node index out of range");
+                }
+            } else {
+                throw new ArgumentOutOfRangeException("context index out of range");
+            }
+        }
+
+        public void AddChild(int context, DSLSymbol child) {
+            if (context < m_children.Length) {
+                m_children[context].Add(child);
+                child.SetParent(this);
+            } else {
+                throw new ArgumentOutOfRangeException("context index out of range");
+            }
+        }
+        
+        
+
+        public override Return Accept<Return, Params>(IASTBaseVisitor<Return,
+            Params> v, params Params[] info) {
+            return v.Visit(this, info);
+        }
+    }
+
+    public class ASTLeaf : DSLSymbol {
+        private string m_stringLiteral;
+
+        public string MStringLiteral => m_stringLiteral;
+
+        public ASTLeaf(string leafLiteral, int mType) :
+            base(mType) {
+            m_stringLiteral = leafLiteral;
+        }
+        public override Return Accept<Return, Params>(IASTBaseVisitor<Return,
+            Params> v, params Params[] info) {
+            return v.Visit(this, info);
+        }
+    }
+}
